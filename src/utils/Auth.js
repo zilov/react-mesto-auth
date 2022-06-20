737fad030c1565031c60c36492ea6d4dacb57117
Danil Zilov <zilov.d@gmail.com>
2022-06-20 13:01:30 +0300

    feat: authentification functions

---
+++ src/utils/Auth.js
@@ -0,0 +1,43 @@
+const authUrl = 'https://auth.nomoreparties.co/'
+
+function checkResponse(res, job) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Ошибка в ${job}`)
+}
+
+export const register = (email, password) => {
+  const job = 'register new user'
+  return fetch(
+    `${authUrl}/signup`, {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({email, password})
+    }.then((response) => {return checkResponse(response, job)})  
+  )
+}
+
+export const login = (email, password) => {
+  const job = 'login a user'
+  return fetch(
+    `${authUrl}/signin`, {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({email, password})
+    }.then((response) => {return checkResponse(response, job)})  
+  )
+}
+
+export const checkToken = (myJwt) => {
+  const job = 'check JWT validity'
+  return fetch(
+    `${authUrl}/users/me`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${myJwt}`
+      }
+    }.then((response) => {return checkResponse(response, job)})  
+  )
+}
