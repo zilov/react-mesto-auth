1cd925a3aba0585de6cffbd505700f2f10ef56a9
Danil Zilov <zilov.d@gmail.com>
2022-06-22 12:39:56 +0300

    feat: add auth popup layout

---
+++ src/components/PopupAuthInfo.js
@@ -0,0 +1,15 @@
+function PopupAuthInfo({name, isOpen, onClose, title, icon}) {
+
+  return(
+    <div 
+      id={`popup-${name}`} 
+      className={`popup ${isOpen && "popup_active"}`}
+      >
+      <div className="popup__window">
+        <button type="button" className="popup__exit-btn" onClick={onClose}></button>
+        <img className="popup__info-icon" src={icon}/>
+        <h2 className="popup__title popup__title_type_auth">{title}</h2>
+      </div>
+    </div>
+  )
+}
